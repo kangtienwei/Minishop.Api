85ac16db6b8e16c245c40a55262a1f7645f0ceb8
tien wei kang <kangtienwei@yahoo.com>
2026-08-19 21:09:17 +0800

    Add project files.

---
+++ Minishop.Api/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Minishop.Api.Data;
+using Minishop.Api.Models;
+
+namespace Minishop.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _context.Products
+                .Where(p => p.IsActive)
+                .ToListAsync();
+
+            return Ok(products);
+        }
+    }
+}
---
+++ Minishop.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Minishop.Api.Models;
+
+namespace Minishop.Api.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
